643df017fe0041cad10637a650d9e1859a534d68
Mike <mishahorod@gmail.com>
2023-12-04 23:56:01 +0200

    second bug fix in comic

--- src/routes/comics/[item]/+page.svelte
+++
@@ -1,8 +0,0 @@
-<script>
-  import SingleComicPage from "../../../components/SingleComicPage/SingleComicPage.svelte";
-  import { page } from "$app/stores";
-
-  let comicId = $page.params.item;
-</script>
-
-<SingleComicPage {comicId} />
